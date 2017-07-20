b5ad8276d8d4c75a7da6bed0ced472fa1b371598
Cristian Bonato <cbonato80@gmail.com>
2017-07-20 12:39:39 +0100

    Introduced counting of trials and correct guesses. -C

--- memory_game/memory.js
+++ memory_game/memory.js
@@ -1,11 +1,16 @@
 
 var c=document.getElementById("myCanvas");
+var eMsg = document.getElementById("p3")
+
 var ctx=c.getContext("2d");
 var tiles = [];
 var NUM_COLS = 4;
 var NUM_ROWS = 3;
 var W = 80;
 var H = 100;
+var index = -1;
+var str1 = 'Number of trials: '
+var str2 = 'Number of correct guesses: '
 
 var Tile = function(x, y, face, name) {
     this.x = x;
@@ -54,32 +59,40 @@ function shuffleArray(array) {
 img0 = createImage('hwu.jpg', 'hwu');
 
 var faces = new Array();
-faces[0] = createImage("images/maxwell.png", 'maxwell');
-faces[1] = createImage("images/curie.png", 'curie');
-faces[2] = createImage("images/bardeen.png", 'bardeen');
-faces[3] = createImage("images/einstein.png", 'einstein');
-faces[4] = createImage("images/franklin.png", 'franklin');
-faces[5] = createImage("images/feynmann.png", 'feynmann');
-faces[6] = createImage("images/cell_phone.png", 'cell_phone');
-faces[7] = createImage("images/smoke_detector.png", 'smoke_detector');
-faces[8] = createImage("images/transistor.png", 'transistor');
-faces[9] = createImage("images/gps.png", 'gps');
-faces[10] = createImage("images/dna.png", 'dna');
-faces[11] = createImage("images/nano.png", 'nano');
+faces[0] = createImage("images/maxwell.png", 0);
+faces[1] = createImage("images/curie.png", 1);
+faces[2] = createImage("images/bardeen.png", 2);
+faces[3] = createImage("images/einstein.png", 3);
+faces[4] = createImage("images/franklin.png", 4);
+faces[5] = createImage("images/feynmann.png", 5);
+faces[6] = createImage("images/cell_phone.png", 0);
+faces[7] = createImage("images/smoke_detector.png", 1);
+faces[8] = createImage("images/transistor.png", 2);
+faces[9] = createImage("images/gps.png", 3);
+faces[10] = createImage("images/dna.png", 4);
+faces[11] = createImage("images/nano.png", 5);
 
 var phys = new Array();
-phys[0] = 'maxwell'
-phys[1] = 'curie'
-phys[2] = 'bardeen'
-phys[3] = 'einstein'
-phys[4] = 'franklin'
-phys[5] = 'feynmann'
-phys[6] = 'maxwell'
-phys[7] = 'curie'
-phys[8] = 'bardeen'
-phys[9] = 'einstein'
-phys[10] = 'franklin'
-phys[11] = 'feynmann'
+phys[0] = 0
+phys[1] = 1
+phys[2] = 2
+phys[3] = 3
+phys[4] = 4
+phys[5] = 5
+phys[6] = 0
+phys[7] = 1
+phys[8] = 2
+phys[9] = 3
+phys[10] = 4
+phys[11] = 5
+
+var msg = new Array();
+msg[0] = 'Correct! The work of James Clerk Maxwell on electromagnetism opened the way to modern telecommunications'
+msg[1] = 'Correct! Some smoke detectors exploit the emission of alpha-particles by Americium-141 to detect smoke. This was enable by the work of Marie Curie on radioactivity.'
+msg[2] = 'Correct! John Bardeen invented the transistor in 1947.'
+msg[3] = 'Correct! The functioning of GPS device requires taking into account corrections fromt he theeory of general relativity, developed by Albert Einstein. '
+msg[4] = 'Correct! Rosalind Franklin made crucial contributions to the understanding of the structure of DNA'
+msg[5] = 'Correct! Richard Feynmann first illustrated the path towards constructing machines at the nano-metric level'
 
 a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
@@ -121,6 +134,7 @@ function getClickPosition(event) {
 var flippedTiles = [];
 var delayStartFC = null;
 var numTries = 0;
+var numCorrect = 0;
 var matchedTiles = [];
 
 mouseClicked = function() {
@@ -139,47 +153,24 @@ mouseClicked = function() {
         if (tiles[i].isUnderMouse(mouseX, mouseY)) {
 
             if (flippedTiles.length < 2 && !tiles[i].isFaceUp) {
+            	document.getElementById("p3").innerHTML = ''
                 tiles[i].drawFaceUp();
                 flippedTiles.push(tiles[i]);
-                //alert (tiles[i].name);
                 if (flippedTiles.length === 2) {
                     numTries++;
+                    document.getElementById("p1").innerHTML = str1.concat(numTries.toString())
                     if (flippedTiles[0].name === flippedTiles[1].name) {
+                    	numCorrect++;
                         flippedTiles[0].isMatch = true;
                         flippedTiles[1].isMatch = true;
                         matchedTiles.push(flippedTiles[0])
                         matchedTiles.push(flippedTiles[1])
+	                    document.getElementById("p2").innerHTML = str2.concat(numCorrect.toString())
+						document.getElementById("p3").innerHTML = msg[flippedTiles[0].name]
                     } 
                     flippedTiles = []
-                    //delayStartFC = frameCount;
-                    //loop();
                 }
             } 
         }
     }
-    
-    //var foundAllMatches = true;
-    //for (var i = 0; i < tiles.length; i++) {
-    //    foundAllMatches = foundAllMatches && tiles[i].isMatch;
-    //}
-    //if (foundAllMatches) {
-    //    fill(0, 0, 0);
-    //    textSize(20);
-    //    text("You found them all in " + numTries + " tries!", 20, 375);
-    //}
 };
-
-/*
-draw = function() {
-    if (delayStartFC && (frameCount - delayStartFC) > 30) {
-        for (var i = 0; i < tiles.length; i++) {
-            if (!tiles[i].isMatch) {
-                tiles[i].drawFaceDown();
-            }
-        }
-        flippedTiles = [];
-        delayStartFC = null;
-    }
-};
-
-*/
